bad953e6b0f5c4e47d530609ad089f08f874ac48
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-12-09 16:49:49 -0700

    initial Commit

---
+++ app/home.js
@@ -0,0 +1,59 @@
+
+
+export default function Home() {
+    return(
+        <main>
+            <div className="membership-table">
+                <div className="memberId">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="startDate">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="expirationDate">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="memberName">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="memberEmail">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="memberPhone">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div className="qrCode">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    );
+}
